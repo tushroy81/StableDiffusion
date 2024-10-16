0c9e7e1b8e616d81b56b7f199fa8bc57805eba71
Tushar Singha Roy <153388292+tushroy81@users.noreply.github.com>
2024-10-17 01:14:31 +0530

    Update training.py

--- training.py
+++ training.py
@@ -22,20 +22,20 @@ save_dir = './weights'
 
 # training hyperparameters
 batch_size = 100
-n_epoch = 32
-lrate=1e-3
+n_epoch = 10
+lrate=0.1
 
 # ======================================================================================
 
 # construct DDPM noise schedule
-b_t = (beta2 - beta1) * torch.linspace(0, 1, timesteps + 1, device=device) + beta1
+b_t = (beta2 - beta1) * torch.linspace(0, 1, timesteps + 1, device=device)
 a_t = 1 - b_t
-ab_t = torch.cumsum(a_t.log(), dim=0).exp()
+ab_t = torch.sum(a_t.log(), dim=0)
 ab_t[0] = 1
 
 # helper function: perturbs an image to a specified noise level
 def perturb_input(x, t, noise):
-    return ab_t.sqrt()[t, None, None, None] * x + (1 - ab_t[t, None, None, None]) * noise
+    return ab_t.sqrt()[t, None, None, None] * noise + (1 - ab_t[t, None, None, None]) * x
 
 # ======================================================================================
 
@@ -45,7 +45,7 @@ optim = torch.optim.Adam(nn_model.parameters(), lr=lrate)
 
 # load dataset and construct optimizer
 dataset = CustomDataset("./sprites_1788_16x16.npy", "./sprite_labels_nc_1788_16x16.npy", transform, null_context=False)
-dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=1)
+dataloader = DataLoader(dataset, batch_size=batch_size, num_workers=1)
 optim = torch.optim.Adam(nn_model.parameters(), lr=lrate)
 
 # set into train mode
@@ -53,9 +53,6 @@ nn_model.train()
 
 for ep in range(n_epoch):
     print(f'epoch {ep}')
-
-    # linearly decay learning rate
-    optim.param_groups[0]['lr'] = lrate*(1-ep/n_epoch)
 
     pbar = tqdm(dataloader, mininterval=2)
     for x, c in pbar:   # x: images  c: context
